a21c52f2d1622a64f9c2d82a9f09d1837fa339e2
Mykyta <76209725+elmalving@users.noreply.github.com>
2024-02-15 16:42:34 +0100

    Update select_3 (analytical_func).sql
    Fixed

--- queries/select_3 (analytical_func).sql
+++ queries/select_3 (analytical_func).sql
@@ -1,4 +1,4 @@
 SELECT
-    RANK() OVER (PARTITION BY skupina ORDER BY datum_narozeni) AS total_rows
+    jmeno, RANK() OVER (PARTITION BY id_skupina ORDER BY datum_narozeni DESC) AS total_rows
 FROM student
 WHERE id_adresa = 2;
